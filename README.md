dac806b37e431aa0ec8a8c11f95ec49f50235bcc
zplush <zahra.hassan@cazoo.co.uk>
2020-07-10 18:14:23 +0100

    create mhfa-notifier app

--- README.md
+++ README.md
@@ -1,53 +1,15 @@
 <p align="center">
   <a href="https://lockbot.app">
-    <img src="https://user-images.githubusercontent.com/10026538/84210020-8ffde000-aaaf-11ea-9568-e579129f4f8a.png">
+    <img src="https://equalengineers.com/wp-content/uploads/2019/01/MHFA1.png">
   </a>
 </p>
 
-# Lockbot
+# MHFA Notifier
 
-![Node.js CI](https://github.com/connorads/lockbot/workflows/Node.js%20CI/badge.svg)
-[![Coverage Status](https://coveralls.io/repos/github/connorads/lockbot/badge.svg?branch=master)](https://coveralls.io/github/connorads/lockbot?branch=master)
-[![Join Slack workspace](https://img.shields.io/badge/slack-join%20workspace-%234A154B)](https://join.slack.com/t/connorads-lockbot/shared_invite/zt-ewpng7t1-Fm78z1SMinWuG_~0DvXa8A)
-![MIT License](https://img.shields.io/github/license/connorads/lockbot)
+## How to use MHFA Notifier
 
-**Demo**: [lockbot.app](https://lockbot.app)
+To notify the MHFA team at Cazoo use the following slash command:
 
-> Coordinate use of your team's shared resources, in Slack 🤝
+**/mhfa**
 
-Lockbot only lets each shared resource be locked by one person at a time - like a Mutex 🔒
-
-_Example use case: One person wants to deploy and test on a shared staging environment without someone else deploying and overwriting their deployed code, so they use `/lock staging-env` to lock the staging environment and notify the channel._
-
-⚠ Lockbot cannot physically prevent naughty or unaware users from using a resource whilst it's locked by someone else.
-Your team must agree to use Lockbot whenever they start or stop using a shared resource.
-
-[![Add Lockbot to Slack](https://platform.slack-edge.com/img/add_to_slack.png)](https://lockbot.app)
-
-## How to use Lockbot
-
-Lockbot has three commands:
-
-- `/locks` Get locked resources list 📜
-- `/lock [resource-name]` Lock a resource 🔒
-- `/unlock [resource-name]` Unlock a resource 🔓
-
-Each Slack channel has its own list of resources.
-
-When someone successfully locks or unlocks a resource, the channel is notified.
-
-## Contributing
-
-Don't be a stranger, contributions are welcome ✨
-
-See the [Contributing Guide](https://github.com/connorads/lockbot/blob/master/CONTRIBUTING.md) for development setup instructions.
-
-- Give this repo a Star ⭐️
-- Ask questions and share your feedback in [issues](https://github.com/connorads/lockbot/issues)
-- Create a PR or issue for bugs, enhancements, ideas and suggestions
-- Join the [Lockbot Community Slack workspace](https://join.slack.com/t/connorads-lockbot/shared_invite/zt-ewpng7t1-Fm78z1SMinWuG_~0DvXa8A)
-- Contact [@connorads](https://connoradams.co.uk)
-
-## Security
-
-If you discover a security vulnerability, please send an e-mail to security@lockbot.app
+the mhfa-support private channel will get notified with the following message: We have received a new message from John Doe
--- src/slack.ts
+++ src/slack.ts
@@ -6,19 +6,10 @@ import {
   SlackCommandMiddlewareArgs,
   SlashCommand,
 } from "@slack/bolt";
-import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { APIGatewayProxyEvent, Context } from "aws-lambda";
 import * as awsServerlessExpress from "aws-serverless-express";
 import * as env from "env-var";
-import LockBot, { Response, Destination } from "./lock-bot";
-import DynamoDBLockRepo from "./storage/dynamodb-lock-repo";
-
-const documentClient = new DocumentClient();
-
-const installationsTableName = env
-  .get("INSTALLATIONS_TABLE_NAME")
-  .required()
-  .asString();
+import fetch from "node-fetch";
 
 const expressReceiver = new ExpressReceiver({
   signingSecret: env.get("SLACK_SIGNING_SECRET").required().asString(),
@@ -26,58 +17,46 @@ const expressReceiver = new ExpressReceiver({
   clientSecret: env.get("SLACK_CLIENT_SECRET").required().asString(),
   stateSecret: env.get("STATE_SECRET").required().asString(),
   scopes: ["commands"],
-  processBeforeResponse: true,
-  installationStore: {
-    storeInstallation: async (installation) => {
-      await documentClient
-        .put({
-          TableName: installationsTableName,
-          Item: {
-            Team: installation.team.id,
-            Installation: installation,
-          },
-        })
-        .promise();
-    },
-    fetchInstallation: async (installQuery) => {
-      const result = await documentClient
-        .get({
-          TableName: installationsTableName,
-          Key: { Team: installQuery.teamId },
-        })
-        .promise();
-      return Promise.resolve(result.Item?.Installation);
-    },
-  },
+  processBeforeResponse: true
 });
 
 const app = new App({
   receiver: expressReceiver,
 });
 
-const getResponseType = (destination: Destination) => {
-  let responseType: "in_channel" | "ephemeral";
-  switch (destination) {
-    case "channel":
-      responseType = "in_channel";
-      break;
-    case "user":
-      responseType = "ephemeral";
-      break;
-  }
-  return responseType;
-};
+export type Destination = "user" | "channel";
+
+export interface Response {
+  message: string;
+  destination: Destination;
+}
 
 const handleResponse = async (
   response: Promise<Response>,
   respond: RespondFn,
-  logger: Logger
+  logger: Logger,
+  text: string,
+  username: string
 ): Promise<void> => {
   try {
-    const { message, destination } = await response;
-    const responseType = getResponseType(destination);
+    const responseType = "ephemeral";
+    const resp = await response;
+    fetch( "https://hooks.slack.com/services/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", {
+      method: 'post',
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+      },
+      body: JSON.stringify({text:`We have received a new message from ${username}`})
+    })
+        .then(function (data) {
+          console.log('Request succeeded with JSON response', data);
+        })
+        .catch(function (error) {
+          console.log('Request failed', error);
+        });
+    //resp.message
     await respond({
-      text: message,
+      text: "Thanks for reaching out. Your message has been highlighted to the Mental Health First Aiders and one of them will be in touch as soon as possible.",
       response_type: responseType,
       as_user: true,
     });
@@ -102,45 +81,22 @@ const handle = (getResponse: (command: SlashCommand) => Promise<Response>) => {
   }) => {
     await ack();
     const response = getResponse(command);
-    await handleResponse(response, respond, logger);
+    await handleResponse(response, respond, logger, command.text, command.user_name);
   };
 };
 
-const lockBot = new LockBot(
-  new DynamoDBLockRepo(
-    documentClient,
-    env.get("RESOURCES_TABLE_NAME").required().asString()
-  )
-);
-
 app.command(
-  "/locks",
-  handle((command) => lockBot.locks(command.channel_id, command.team_id))
-);
-app.command(
-  "/lock",
-  handle((command) =>
-    lockBot.lock(
-      command.text,
-      `<@${command.user_id}>`,
-      command.channel_id,
-      command.team_id
-    )
-  )
-);
-app.command(
-  "/unlock",
-  handle((command) =>
-    lockBot.unlock(
-      command.text,
-      `<@${command.user_id}>`,
-      command.channel_id,
-      command.team_id
-    )
-  )
+    "/mhfa",
+    handle((command) => {
+      return Promise.resolve({
+        message: "Someone will get in touch with you",
+        destination: "user",
+      } as Response);
+    })
 );
 
 const server = awsServerlessExpress.createServer(expressReceiver.app);
 const handler = (event: APIGatewayProxyEvent, context: Context) =>
   awsServerlessExpress.proxy(server, event, context);
 exports.handler = handler;
+
