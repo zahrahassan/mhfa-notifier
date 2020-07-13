45b35714cb6ca5fec936ed1f4ec15d2ff995850b
Connor Adams <connorads@users.noreply.github.com>
2020-07-13 19:04:28 +0100

    Update formatting

--- src/slack.ts
+++ src/slack.ts
@@ -1,7 +1,4 @@
-import {
-  App,
-  ExpressReceiver,
-} from "@slack/bolt";
+import { App, ExpressReceiver } from "@slack/bolt";
 import { APIGatewayProxyEvent, Context } from "aws-lambda";
 import * as awsServerlessExpress from "aws-serverless-express";
 import * as env from "env-var";
@@ -20,40 +17,42 @@ const app = new App({
 
 const slackHookUrl = env.get("SLACK_HOOK_URL").required().asString();
 
-app.command(
-  "/mhfa", async ({ logger, command, ack, respond, }) => {
-    ack();
-    try {
-      const slackHookResponse = await fetch(slackHookUrl, {
-        method: 'post',
-        headers: {
-          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-        },
-        body: JSON.stringify({ text: `We have received a new message from ${command.user_name}` })
-      })
-      logger.info(slackHookResponse);
-      if (slackHookResponse.ok) {
-        await respond({
-          text: "Thanks for reaching out. Your message has been highlighted to the Mental Health First Aiders and one of them will be in touch as soon as possible.",
-          response_type: "ephemeral",
-          as_user: true,
-        });
-      } else {
-        throw Error(`Slack Hook response was not 200: ${slackHookResponse.status}`)
-      }
-    } catch (e) {
-      logger.error(e);
+app.command("/mhfa", async ({ logger, command, ack, respond }) => {
+  ack();
+  try {
+    const slackHookResponse = await fetch(slackHookUrl, {
+      method: "post",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        text: `We have received a new message from ${command.user_name}`,
+      }),
+    });
+    logger.info(slackHookResponse);
+    if (slackHookResponse.ok) {
       await respond({
-        text: `We're sorry, something went wrong, please try again.`,
+        text:
+          "Thanks for reaching out. Your message has been highlighted to the Mental Health First Aiders and one of them will be in touch as soon as possible.",
         response_type: "ephemeral",
         as_user: true,
       });
+    } else {
+      throw Error(
+        `Slack Hook response was not 200: ${slackHookResponse.status}`
+      );
     }
+  } catch (e) {
+    logger.error(e);
+    await respond({
+      text: `We're sorry, something went wrong, please try again.`,
+      response_type: "ephemeral",
+      as_user: true,
+    });
   }
-);
+});
 
 const server = awsServerlessExpress.createServer(expressReceiver.app);
 const handler = (event: APIGatewayProxyEvent, context: Context) =>
   awsServerlessExpress.proxy(server, event, context);
 exports.handler = handler;
-
